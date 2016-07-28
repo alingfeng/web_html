0a75039729577c0eff0254d4a6f1c99f1a7adc33
alinfeng <14293652452@qq.com>
2016-07-28 17:43:02 +0800

    change5 drag.js

--- drag.js
+++ drag.js
@@ -3,7 +3,10 @@ function drag(id){
 	var obj = document.getElementById(id);
 	var disX = 0;
 	var disY = 0;
-	obj.onmousedown = function(){
+	obj.onmousedown = function(ev){
+		var ev = ev || event;
+		disX = ev.clientX - this.offsetLeft;
+		disY = ev.clientY - this.offsetTop;
 	
 	}
 }
