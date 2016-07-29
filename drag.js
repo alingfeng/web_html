d129bbe3a6169c735379c8528d992d29e8e8579c
alinfeng <14293652452@qq.com>
2016-07-29 09:25:44 +0800

    change drag.js form master

--- drag.js
+++ drag.js
@@ -7,7 +7,13 @@ function drag(id){
 		var ev = ev || event;
 		disX = ev.clientX - this.offsetLeft;
 		disY = ev.clientY - this.offsetTop;
-	
+		
+		document.onmouseomove = function(ev){
+			var ev = ev || event;
+
+			obj.style.left = ev.clientX - disX +'px';
+			obj.style.top = ev.clientY - disY +'px';
+		}
 	}
 }
 
