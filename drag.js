aca2df9f6473f204dd08e0e37434d2a5d557ce01
alinfeng <14293652452@qq.com>
2016-07-29 09:42:35 +0800

    change drag.js from new1

--- drag.js
+++ drag.js
@@ -7,7 +7,12 @@ function drag(id){
 		var ev = ev || event;
 		disX = ev.clientX - this.offsetLeft;
 		disY = ev.clientY - this.offsetTop;
-	
+		document.onmouseomove = function(ev){
+			var ev = ev || event;
+
+			obj.style.left = ev.pageX - disX +'px';
+			obj.style.top = ev.pageY - disY +'px';
+		}
 	}
 }
 
